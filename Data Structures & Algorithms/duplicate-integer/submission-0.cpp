25b23d25fefdfd2616c756ad879a4ad58c8803c0
LeoMart12345 <121646499+LeoMart12345@users.noreply.github.com>
2026-06-12 14:53:20 +0100

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::set<int> set;
+
+        for(int i = 0; i < nums.size(); ++i){
+            if(set.find(nums[i]) != set.end()){
+                return true;
+                
+            }
+            set.insert(nums[i]);
+        }
+        return false;
+    }
+};
