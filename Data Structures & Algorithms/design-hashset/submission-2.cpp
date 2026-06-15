1bbdc3f9e5128d8800fb254a21350e5b50544a15
LeoMart12345 <121646499+LeoMart12345@users.noreply.github.com>
2026-06-15 17:56:25 +0100

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.cpp
@@ -0,0 +1,41 @@
+class MyHashSet {
+private:
+    static const int BUCKET = 10000;
+    std::vector<std::list<int>> buckets;
+
+    int hash(int key) const{
+        return key % BUCKET;
+    }
+
+public:
+    MyHashSet() : buckets(BUCKET){}
+    
+    void add(int key) {
+        int idx = hash(key);
+        for(int x : buckets[idx]){
+            if(x == key) return;
+        }
+        buckets[idx].push_back(key);
+    }
+    
+    void remove(int key) {
+        int idx = hash(key);
+        buckets[idx].remove(key);
+    }
+    
+    bool contains(int key) {
+        int idx = hash(key);
+        for(int x : buckets[idx]){
+            if(x == key) return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
