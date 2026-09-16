82a3d14aa7f8074a4b5c7e36a2293ef19668ebb7
LeoMart12345 <121646499+LeoMart12345@users.noreply.github.com>
2026-09-16 13:49:35 +0100

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> st;
+
+        for(char c : s){
+            if(c == '{' or c == '[' or c == '('){
+                st.push(c);
+            }
+            else{
+                if(st.empty()) return false;
+
+                char top = st.top();
+                
+                if(c == '}' && top != '{' or 
+                c == ']' && top != '[' or 
+                c == ')' && top != '(')
+                {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
